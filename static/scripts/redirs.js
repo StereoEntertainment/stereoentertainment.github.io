0f0344d66947391f59ce0c33bcdaf969e0406cb4
Dark <126415484+DarkCookieSF@users.noreply.github.com>
2026-07-16 22:44:06 -0700

    actually made the webapge, broken

---
+++ static/scripts/redirs.js
@@ -0,0 +1,15 @@
+function homeRedir() {
+    location.href = "../../index.html";
+}
+
+function ourteamRedir() {
+    location.href = "../../pages/ourteam.html";
+}
+
+function socialsRedir() {
+    location.href = "../../pages/socials.html";
+}
+
+function discordRedir() {
+    location.href = "https://discord.gg/HgT5sWJQBh";
+}
